2668efdf03a6f075d57f58b507f5e00e16808e0a
Gabriel Jordan <gabrijordan3@gmail.com>
2025-06-29 14:02:18 -0300

    adicioanr hover no componente de botao

--- src/components/shared/button.tsx
+++ src/components/shared/button.tsx
@@ -5,7 +5,7 @@ type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
 };
 
 const baseStyles =
-    "inline-flex items-center justify-center rounded-lg font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2";
+    "inline-flex items-center justify-center rounded-lg font-medium  hover:cursor-pointer transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2";
 
 const variantStyles: Record<string, string> = {
     primary: "bg-blue-600 text-white hover:bg-blue-700 focus:ring-blue-500",
